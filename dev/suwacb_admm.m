5b5b7194b3704d38f20a17b36b4be155800529e3
Yuki Itoh <yukiitohand@gmail.com>
2020-11-03 18:21:45 -0500

    preselected endmember functionality is added

--- dev/suwacb_admm.m
+++ dev/suwacb_admm.m
@@ -9,6 +9,8 @@ function [ XA,XC,res,Ctmp ] = suwacb_admm( x,A,Y,varargin )
 %   Optional parameters
 %       'TOL': scalar, tolerance, (default) 0.048
 %       'MAXNUM': scalar, maximal number of endmembers, (default) 4
+%       'PRESELECTED_IDX': logical or numerical array
+%            indicies for the preselected endmembers, (default) []
 %   [future implementation]
 %       'BLOCKSIZE': scaler, number of vectors that are thrown to cvx 
 %                    solverat one time
@@ -16,17 +18,19 @@ function [ XA,XC,res,Ctmp ] = suwacb_admm( x,A,Y,varargin )
 %   Output variables
 %       XA: (p x N)-array, estimated abundance matrix
 %       XC: (d x N)-array, estimated concave curve matrix
-%       sp_noise: (d x N)-array, estimated sparse noise
 %       res: (1 x N)-array, residuals
 
 tol = 0.044;
 maxnum = 4;
+idx_sel_init = [];
 
 if (rem(length(varargin),2)==1)
     error('Optional parameters should always go by pairs.');
 else
     for i=1:2:(length(varargin)-1)
         switch upper(varargin{i})
+            case 'PRESELECTED_IDX'
+                idx_sel_init = varargin{i+1};
             case 'TOL'
                 tol = varargin{i+1};
                 if (tol<=0)
@@ -38,71 +42,90 @@ else
                        error('MAXNUM must be positive.');
                 end
             otherwise
-                % Hmmm, something wrong with the parameter string
-                error(['Unrecognized option: ''' varargin{i} '''']);
-        end;
-    end;
+                error('Unrecognized option: %s',varargin{i});
+        end
+    end
 end
 
-[d,p] = size(A);
-N = size(Y,2);
+[d,p] = size(A); N = size(Y,2);
 
-% L = zeros([d,d]);
-% for i=2:d-1
-%     L(i,i) = -1;
-%     L(i,i-1) = (x(i+1)-x(i)) / (x(i+1)-x(i-1));
-%     L(i,i+1) = (x(i)-x(i-1)) / (x(i+1)-x(i-1));
-% end
-% L = L(2:end-1,:);
+if ~isempty(idx_sel_init)
+    if islogical(idx_sel_init)
+        if length(idx_sel_init) ~= p
+            error('The size of PRESELECTED_IDX is not right');
+        end
+        idx_sel_init = reshape(find(idx_sel_init),1,[]);
+    end
+    idx_res_init = setdiff(1:p,idx_sel_init);
+else
+    idx_sel_init = []; idx_res_init = 1:p;
+end
 
-XA = zeros(p,N);
-XC = zeros(d,N);
-res = zeros(1,N);
 
+
+XA = zeros(p,N); XC = zeros(d,N); res = zeros(1,N);
 
 for n=1:N %numblocks
-    tic;
     y = Y(:,n);
-    flg = 1;
-    % first iteration without any endmembers
-    % [ xC,r ] = ConcaveFit( x,y );
-    [~,xC,Ctmp] =  huwacb_admm2_stflip([],y,x);
-    r = vnorms(y- Ctmp*xC,1,2);
-    fprintf('Initial Error: %f\n',r);
-    if r<tol
-        % finish if the residual is below the tolerance
-        flg = 0; XA(:,n) = 0; XC(:,n) = xC; res(n) = r;
+    % ------------------------------------------------------------------- %
+    % first iteration with pre-selected endmembers
+    % ------------------------------------------------------------------- %
+    % start from pre-selected endmembers
+    idx_sel = idx_sel_init; idx_res = idx_res_init;
+    Atmp = A(:,idx_sel);
+    
+    % Perform a constrained non-negative least squares with pre-selected
+    % endmembers
+    [xA_min,xC_min,Ctmp] =  huwacb_admm2_stflip(Atmp,y,x);
+    
+    % Obtain residual
+    if isempty(xA_min)
+        r_min = vnorms(y - Ctmp*xC_min,1,2);
+    else
+        r_min = vnorms(y - Atmp*xA_min - Ctmp*xC_min,1,2);
     end
-    idx_sel = []; idx_res = 1:p; iter = 0;
-    while flg
+    
+    fprintf('Initial Error: %f\n',r_min);
+    
+    % ------------------------------------------------------------------- %
+    % Main Loop
+    % ------------------------------------------------------------------- %
+    iter = 0;
+    % Aggregate endmembers if the error "r_min" is greater than tolerance
+    % or until the maximum number of the additional endmembers is reached.
+    while (r_min>tol) && (iter < maxnum)
         iter = iter + 1;
-        r_min=inf; i_r_min = 0;rList = [];
+        
+        % Find the endmember that minimizes the error in a least square
+        % sense.
+        r_min=inf; i_r_min = 0;
         for i=idx_res
+            % increment an endmember
             Atmp = [A(:,idx_sel) A(:,i)];
-%             [xA, xC, sp_noise,r] = huwacb_spnoise( x,Atmp,y,'GAMMA',0.20 );
-            % [xA, xC,r] = huwacb( x,Atmp,y );
+            
+            % Perform a constrained non-negative least squares with the
+            % incremented endmember.
             [xA,xC,Ctmp] =  huwacb_admm2_stflip(Atmp,y,x);
+            
+            % Obtain residual
             r = vnorms(y - Atmp*xA - Ctmp*xC,1,2);
-            rList = [rList r];
+            
+            % if the obtained error is smaller than current minimizer,
+            % exchange the candidate.
             if r<r_min
                 r_min = r; i_r_min = i; xA_min = xA; xC_min = xC;
-%                 sp_noise_min = sp_noise;
             end
         end
         fprintf('Iter:%d, Index:%d selected\nError: %f\n',iter,i_r_min,r_min);
+        
+        % Add the endmember that reduces the error most.
         idx_sel = [idx_sel i_r_min];
         idx_res = setdiff(idx_res,i_r_min);
-        if r_min<tol
-            % finish if the residual is below the tolerance
-            flg = 0; XA(idx_sel,n) = xA_min; XC(:,n) = xC_min;
-            res(n) = r_min; % sp_noise = sp_noise_min;
-        end
-        if length(idx_sel) >= maxnum
-            % finish if the residual is below the tolerance
-            flg = 0; XA(idx_sel,n) = xA_min; XC(:,n) = xC_min;
-            res(n) = r_min; % sp_noise = sp_noise_min;
-        end
     end
+    
+    if ~isempty(xA_min), XA(idx_sel,n) = xA_min; end
+    
+    XC(:,n) = xC_min; res(n) = r_min;
     
 end
 
